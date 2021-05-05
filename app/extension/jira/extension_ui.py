72313782012b0c928f21068aa84b8f166121732b
Hadi Abou Hamzeh <67643814+hadiabouhamze@users.noreply.github.com>
2021-05-05 15:34:24 +0300

    OTJ-1375

--- app/extension/jira/extension_ui.py
+++ app/extension/jira/extension_ui.py
@@ -8,14 +8,14 @@ from selenium_ui.jira.pages.pages import Login
 from util.conf import JIRA_SETTINGS
 
 
-def app_specific_action_auto_update_configuration(webdriver, datasets):
-    page = BasePage(webdriver)
-
-    @print_timing("selenium_app_custom_action_auto_update_configuration")
-    def measure():
-        page.go_to_url(f"{JIRA_SETTINGS.server_url}/plugins/servlet/admin/ftp")
-        page.wait_until_visible((By.ID, "admin"))
-    measure()
+# def app_specific_action_auto_update_configuration(webdriver, datasets):
+#     page = BasePage(webdriver)
+#
+#     @print_timing("selenium_app_custom_action_auto_update_configuration")
+#     def measure():
+#         page.go_to_url(f"{JIRA_SETTINGS.server_url}/plugins/servlet/admin/ftp")
+#         page.wait_until_visible((By.ID, "admin"))
+#     measure()
 
 
 # def app_specific_action_language_configuration(webdriver, datasets):
@@ -38,13 +38,13 @@ def app_specific_action_otj_client(webdriver, datasets):
     measure()
 
 
-# def app_specific_action_otj_data(webdriver, datasets):
-#     page = BasePage(webdriver)
-#
-#     @print_timing("selenium_app_custom_action_otj_data")
-#     def measure():
-#         page.go_to_url(f"{JIRA_SETTINGS.server_url}/plugins/servlet/admin/otjdata")
-#         page.wait_until_visible((By.ID, "resources"))
-#     measure()
+def app_specific_action_otj_data(webdriver, datasets):
+    page = BasePage(webdriver)
+
+    @print_timing("selenium_app_custom_action_otj_data")
+    def measure():
+        page.go_to_url(f"{JIRA_SETTINGS.server_url}/plugins/servlet/admin/otjdata")
+        page.wait_until_visible((By.ID, "resources"))
+    measure()
 
 
