0be0bc9f1ed77c192153067499702b1b48c45b5d
Hadi Abou Hamzeh <67643814+hadiabouhamze@users.noreply.github.com>
2021-06-18 13:08:11 +0300

    JTC-196

--- app/extension/jsm/extension_ui_agents.py
+++ app/extension/jsm/extension_ui_agents.py
@@ -11,24 +11,19 @@ def app_specific_action(webdriver, datasets):
     if datasets['custom_issues']:
         issue_key = datasets['custom_issue_key']
 
-    # To run action as specific user uncomment code bellow.
-    # NOTE: If app_specific_action is running as specific user, make sure that app_specific_action is running
-    # just before test_2_selenium_z_log_out action
+    def app_specific_action_log_level_web_action(webdriver, datasets):
+        page = BasePage(webdriver)
 
-    # @print_timing("selenium_app_specific_user_login")
-    # def measure():
-    #     def app_specific_user_login(username='admin', password='admin'):
-    #         login_page = Login(webdriver)
-    #         login_page.delete_all_cookies()
-    #         login_page.go_to()
-    #         login_page.set_credentials(username=username, password=password)
-    #         if login_page.is_first_login():
-    #             login_page.first_login_setup()
-    #         if login_page.is_first_login_second_page():
-    #             login_page.first_login_second_page_setup()
-    #         login_page.wait_for_page_loaded()
-    #     app_specific_user_login(username='admin', password='admin')
-    # measure()
+        @print_timing("selenium_agent_app_custom_action_log_level_web_action")
+        def measure():
+            @print_timing("selenium_agent_app_custom_action:log_level_web_action")
+            def sub_measure():
+                page.go_to_url(f"{JSM_SETTINGS.server_url}/secure/LogLevel.jspa")
+                page.wait_until_visible((By.ID, "system-rules-table"))
+
+            sub_measure()
+
+        measure()
 
     @print_timing("selenium_agent_app_custom_action")
     def measure():
