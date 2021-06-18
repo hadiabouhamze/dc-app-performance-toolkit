0935672c372da5d71ef909b7880be2ca7c7357ba
Hadi Abou Hamzeh <67643814+hadiabouhamze@users.noreply.github.com>
2021-06-18 14:47:18 +0300

    Revert "JTC-196"
    This reverts commit c8d73e4e7cc8198228db81988484d60f4a2b4903.

--- app/extension/jsm/extension_ui_agents.py
+++ app/extension/jsm/extension_ui_agents.py
@@ -14,7 +14,7 @@ def app_specific_action_log_level_web_action(webdriver, datasets):
         @print_timing("selenium_agent_app_custom_action:log_level_web_action")
         def sub_measure():
             page.go_to_url(f"{JSM_SETTINGS.server_url}/secure/LogLevel.jspa")
-            page.wait_until_visible((By.ID, "log-level-main-panel"))
+            page.wait_until_visible((By.ID, "log-level-panel"))
 
         sub_measure()
 
